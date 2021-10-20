0bfa6350b9e844883b4ff13cf7a51edff56be713
Héctor de la Rosa <hoftherose@gmail.com>
2021-10-20 14:27:59 -0500

    Add error messages

---
+++ src/utils/errors.py
@@ -0,0 +1,10 @@
+"""Error definitions for system"""
+
+class AudioDurationMismatchError(Exception):
+    """Error for when audio duration is not what was expected"""
+
+class AudioTypeMismatchError(Exception):
+    """Error when audio type is not what was expected"""
+
+class AudioSampleRateMismatchError(Exception):
+    """Error when audio sample rate is not what was expected"""
